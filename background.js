0c077eeeaf794da785a82400f53f056a347bf6c9
Saleh Saiid <saleh.saiid@gmail.com>
2014-12-13 05:05:15 +0200

    set onUpdated listener to get facebook tabs and save opend time

--- background.js
+++ background.js
@@ -1,3 +1,27 @@
+var fbTabs = {};
+/*
 chrome.tabs.query({url : "*://*.facebook.com/*"}, function(tabs_array){
         console.log(tabs_array);
 });
+*/
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+	if(changeInfo.url){
+		console.log(changeInfo);
+		url = changeInfo.url;
+		pattern = /https?\:\/\/(?:www\.)?facebook\.com\/\/?/;
+		ma = pattern.exec(url);
+		//console.log(ma);
+		if(ma){
+			if(!fbTabs[tabId]){
+				fbTabs[tabId] = new Date().getTime();
+			}
+		}
+	}
+i});
+
+var time = setInterval(function(){
+	for(tabId in fbTabs){
+	
+	}
+}, 2000);
